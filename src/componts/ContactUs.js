974c1420dfc62fc5eb82037dde2ee112748abb56
HamidAkhtar00 <141080791+HamidAkhtar00@users.noreply.github.com>
2024-02-03 18:58:15 +0500

    here i have added two components Home and Contact us page  in src folder

---
+++ src/componts/ContactUs.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ContactUs() {
+    return (
+        <>
+            <p>This is Contact us page</p>
+        </>
+    )
+}
+
+export default ContactUs
---
+++ src/componts/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+    return (
+        <>
+            <p>Hellow world from home page</p>
+        </>
+    )
+}
+
+export default Home
